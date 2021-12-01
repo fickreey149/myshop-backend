ff86037191c505e42c36f1f25a0b76f2cdb5dca8
Fikri Rahman Hidayat <fickreeyhidayat@gmail.com>
2021-12-01 12:11:00 +0700

    Backend MyShop

---
+++ app/Http/Controllers/API/TransactionController.php
@@ -0,0 +1,74 @@
+<?php
+
+namespace App\Http\Controllers\API;
+
+use App\Helpers\ResponseFormatter;
+use App\Http\Controllers\Controller;
+use App\Http\Requests\CheckoutRequest;
+use App\Models\Transaction;
+use App\Models\TransactionItem;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+
+class TransactionController extends Controller
+{
+    public function all(Request $request)
+    {
+        $id = $request->input('id');
+        $limit = $request->input('limit');
+        $status = $request->input('status');
+
+        if ($id) {
+            $transaction = Transaction::with(['items.product'])->find($id);
+
+            if ($transaction) {
+                return ResponseFormatter::success(
+                    $transaction,
+                    'Data transaksi berhasil diambil'
+                );
+            } else {
+                return ResponseFormatter::error(
+                    null,
+                    'Data transaksi tidak ada',
+                    404
+                );
+            }
+        }
+
+        $transaction = Transaction::with(['items.product'])->where('users_id', Auth::user()->id);
+
+        if ($status) {
+            $transaction->where('status', $status);
+        }
+
+        return ResponseFormatter::success(
+            $transaction->paginate($limit),
+            'Data list transaksi berhasil diambil'
+        );
+    }
+
+    public function checkout(CheckoutRequest $request)
+    {
+        $transaction = Transaction::create([
+            'users_id' => Auth::user()->id,
+            'address' => $request->address,
+            'total_price' => $request->total_price,
+            'shipping_price' => $request->shipping_price,
+            'status' => $request->status
+        ]);
+
+        foreach ($request->items as $item) {
+            TransactionItem::create([
+                'users_id' => Auth::user()->id,
+                'products_id' => $item['id'],
+                'transactions_id' => $transaction->id,
+                'quantity' => $item['quantity'],
+            ]);
+        }
+
+        return ResponseFormatter::success(
+            $transaction->load('items.product'),
+            'Transaksi Berhasil'
+        );
+    }
+}
---
+++ app/Http/Controllers/API/UserController.php
@@ -0,0 +1,97 @@
+<?php
+
+namespace App\Http\Controllers\API;
+
+use App\Helpers\ResponseFormatter;
+use App\Http\Controllers\Controller;
+use App\Http\Requests\LoginRequest;
+use App\Http\Requests\RegisterRequest;
+use Illuminate\Http\Request;
+use App\Models\User;
+use Exception;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Facades\Auth;
+
+class UserController extends Controller
+{
+    public function register(RegisterRequest $request)
+    {
+        try {
+            User::create([
+                'name' => $request->name,
+                'username' => $request->username,
+                'email' => $request->email,
+                'phone' => $request->phone,
+                'password' => Hash::make($request->password),
+            ]);
+
+            $user = User::where('email', $request->email)->first();
+
+            $token_result = $user->createToken('authToken')->plainTextToken;
+
+            return ResponseFormatter::success([
+                'access_token' => $token_result,
+                'token_type' => 'Bearer',
+                'user' => $user,
+            ], 'User Registered');
+        } catch (Exception $error) {
+            return ResponseFormatter::error([
+                'message' => 'Something went wrong',
+                'error' => $error,
+            ], 'Authentication Failed', 500);
+        }
+    }
+
+    public function login(LoginRequest $request)
+    {
+        try {
+            $credentials = request(['email', 'password']);
+            if (!Auth::attempt($credentials)) {
+                return ResponseFormatter::error([
+                    'message' => 'Unauthorized'
+                ], 'Authentication Failed', 500);
+            }
+
+            $user = User::where('email', $request->email)->first();
+
+            if (!Hash::check($request->password, $user->password, [])) {
+                throw new \Exception('Invalid Credential');
+            }
+
+            $tokenResult = $user->createToken('authToken')->plainTextToken;
+
+            return ResponseFormatter::success([
+                'access_token' => $tokenResult,
+                'token_type' => 'Bearer',
+                'user' => $user,
+            ], 'Authenticated');
+        } catch (Exception $error) {
+            return ResponseFormatter::error([
+                'message' => 'Something went wrong',
+                'error' => $error,
+            ], 'Authentication Failed', 500);
+        }
+    }
+
+    public function fetch(Request $request)
+    {
+        return ResponseFormatter::success($request->user(), 'Data Profil User berhasil diambil');
+    }
+
+    public function updateProfile(Request $request)
+    {
+        $data = $request->all();
+
+        $user = Auth::user();
+        $user->update($data);
+
+        return ResponseFormatter::success($user, 'Profile Updated');
+    }
+
+    public function logout(Request $request)
+    {
+        $token = $request->user()->currentAccessToken()->delete();
+
+        return ResponseFormatter::success($token, 'Token Revoked');
+    }
+}
